adb838fb88ffc3eeaad9d9a227de1e7e24e9b3c9
sumant2000 <sumant2000@gmail.com>
2024-10-29 07:28:56 +0530

    first code to learn go

---
+++ HelloWorld.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+
+func main() {
+	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
+	// to see how GoLand suggests fixing it.
+	s := "gopher"
+	fmt.Println("Hello and welcome, %s!", s)
+
+}
+
+//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
